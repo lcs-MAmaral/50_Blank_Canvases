00768748dede1c2d7cb383fe01146f3f04f23e52
Matthew Amaral <mamaral@lcs-mba3204.lcs.on.ca>
2017-11-14 11:01:09 -0500

    Got a triangle

---
+++ 50 Blank Canvases.playground/Pages/Triangle.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//: [Previous](@previous) / [Next](@next)
+//: # A Blank Canvas
+//:
+//: Use this page to experiment. Have fun!
+/*:
+ ## Required code
+ 
+ The following statements are required to make the playground run.
+ 
+ Please do not remove.
+ */
+import Cocoa
+import PlaygroundSupport
+
+// Create canvas
+let canvas = Canvas(width: 400, height: 300)
+let x = 0
+let y = 0
+let a = 300
+
+canvas.defaultLineWidth = 5
+canvas.drawLine(fromX: x, fromY: y, toX: x + a, toY: y)
+canvas.translate(byX: x + a, byY: y)
+canvas.rotate(by: 120)
+canvas.drawLine(fromX: x, fromY: y, toX: x + a, toY: y)
+canvas.translate(byX: x + a, byY: y)
+canvas.rotate(by: 120)
+canvas.drawLine(fromX: x, fromY: y, toX: x + a, toY: y)
+
+PlaygroundPage.current.liveView = canvas.imageView
+
